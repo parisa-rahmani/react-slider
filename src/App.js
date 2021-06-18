82966a98b08c5f022a221578bd1456d5069b760b
parisa-rahmani <parisa.rahmani998@gmail.com>
2021-06-19 00:00:55 +0430

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useCallback, useEffect, useState } from 'react';
+import { data } from './data';
 
-function App() {
+const App = () => {
+  const [slides, setSlides] = useState(data);
+  const [index, setIndex] = useState(0);
+
+  const getNextSlide = useCallback(() => {
+    setIndex(oldIndex => {
+      let index = oldIndex + 1;
+      if (index > slides.length - 1) {
+        index = 0;
+      }
+      return index;
+    });
+  }, [slides.length]);
+
+  const getPrevSlide = () => {
+    if (index > 0) {
+      setIndex(index => index - 1);
+    } else {
+      setIndex(slides.length - 1);
+    }
+    return index;
+  };
+
+  // index in dependency because every time index change manualy 3 sec start over this is correct
+  useEffect(() => {
+    const interval = setInterval(() => {
+      getNextSlide();
+    }, 3000);
+
+    return () => clearInterval(interval);
+  }, [index, getNextSlide]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="slider-container">
+      {slides.map((slide, slideIndex) => {
+        const { id, name, image, desc } = slide;
+        let position = 'nextSlide';
+        if (slideIndex === index) position = 'activeSlide';
+        if (
+          slideIndex === index - 1 ||
+          (index === 0 && slideIndex === slides.length - 1)
+        )
+          position = 'prevSlide';
+        return (
+          <article className={position} key={id}>
+            <img className="slide-img" src={image} alt={name} />
+            <h2>{name}</h2>
+            <p>{desc}</p>
+          </article>
+        );
+      })}
+
+      <button className="next" onClick={getNextSlide}>
+        next
+      </button>
+      <button className="prev" onClick={getPrevSlide}>
+        prev
+      </button>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,30 @@
+export const data = [
+  {
+    id: 1,
+    name: 'aysan ian',
+    image:
+      'https://i.pinimg.com/474x/12/3c/82/123c825e43ed7288fbd3cc3abcb56300.jpg',
+    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa molestiae ipsa, a at asperiores libero, facere tenetur illum sit fuga totam sed minus. Voluptas aut dolor beatae officia perspiciatis fuga!',
+  },
+  {
+    id: 2,
+    name: 'john harvay',
+    image:
+      'https://i.pinimg.com/236x/07/33/ba/0733ba760b29378474dea0fdbcb97107.jpg',
+    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa molestiae ipsa, a at asperiores libero, facere tenetur illum sit fuga totam sed minus. Voluptas aut dolor beatae officia perspiciatis fuga!',
+  },
+  {
+    id: 3,
+    name: 'susan irnvi',
+    image:
+      'https://i.pinimg.com/474x/5b/2e/d9/5b2ed94597e71e2661a244df763cc324.jpg',
+    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa molestiae ipsa, a at asperiores libero, facere tenetur illum sit fuga totam sed minus. Voluptas aut dolor beatae officia perspiciatis fuga!',
+  },
+  {
+    id: 4,
+    name: 'stefan libert',
+    image:
+      'https://i.pinimg.com/736x/38/93/07/389307d6af5c4be0051b7d3c4f93bf3d.jpg',
+    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa molestiae ipsa, a at asperiores libero, facere tenetur illum sit fuga totam sed minus. Voluptas aut dolor beatae officia perspiciatis fuga!',
+  },
+];
